49b43d9505bcc6810b82d3e096d1cd265ce0bd3b
michal <depodefi@gmail.com>
2016-03-07 14:51:38 +0100

    second commit

--- app/src/main/java/hackathon/glass/MainActivity.java
+++ app/src/main/java/hackathon/glass/MainActivity.java
@@ -1,10 +1,5 @@
 package hackathon.glass;
 
-import com.google.android.glass.media.Sounds;
-import com.google.android.glass.widget.CardBuilder;
-import com.google.android.glass.widget.CardScrollAdapter;
-import com.google.android.glass.widget.CardScrollView;
-
 import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
@@ -12,6 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+
+import com.google.android.glass.media.Sounds;
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
 
 /**
  * An {@link Activity} showing a tuggable "Hello World!" card.
